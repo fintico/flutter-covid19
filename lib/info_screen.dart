23c3a07e29b182cd2b85bc3a98ab254b95cd3489
Edwin Guerrero <edwin@acchiwan.pe>
2020-04-25 15:41:34 -0500

    Se culmina la creación del card para prevencion

--- lib/info_screen.dart
+++ lib/info_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_covid_app/constant.dart';
 import 'package:flutter_covid_app/widgets/header.dart';
+import 'package:flutter_covid_app/widgets/symthomp_card.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class InfoScreen extends StatelessWidget {
   @override
@@ -18,6 +20,7 @@ class InfoScreen extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Text(
                   "Sintomas",
@@ -27,30 +30,85 @@ class InfoScreen extends StatelessWidget {
                   height: 20,
                 ),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                offset: Offset(0, 10),
-                                blurRadius: 20,
-                                color: kActiveShadowColor),
-                          ]),
-                      child: Column(
-                        children: <Widget>[
-                          Image.asset("assets/images/headache.png", height: 90,),
-                          Text(
-                            "Dolor Cabeza",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 10),
-                          ),
-                        ],
-                      ),
+                    SympthomCards(
+                      image: "assets/images/headache.png",
+                      title: "Dolor Cabeza",
+                      isActive: true,
+                    ),
+                    SympthomCards(
+                      image: "assets/images/caugh.png",
+                      title: "Tos seca",
+                    ),
+                    SympthomCards(
+                      image: "assets/images/fever.png",
+                      title: "Fiebre",
                     ),
                   ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Como Prevenir",
+                  style: kTitleTextstyle.copyWith(fontSize: 16),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 156,
+                  child: Stack(
+                    alignment: Alignment.centerLeft,
+                    children: <Widget>[
+                      Container(
+                        height: 136,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(0, 8),
+                                blurRadius: 24,
+                                color: kShadowColor,
+                              ),
+                            ]),
+                      ),
+                      Image.asset("assets/images/wash_hands.png"),
+                      Positioned(
+                        left: 136,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 15),
+                          width: MediaQuery.of(context).size.width - 170,
+                          height: 136,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "Lavate las manos",
+                                style: kTitleTextstyle,
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Desde que inicio Covid19 se ha comprobado que el lavado de manos ayuda.",
+                                style: TextStyle(fontSize: 12),
+                              ),
+                              Align(
+                                alignment: Alignment.bottomRight,
+                                child: SvgPicture.asset(
+                                    "assets/icons/forward.svg"),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
---
+++ lib/widgets/symthomp_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_covid_app/constant.dart';
+
+class SympthomCards extends StatelessWidget {
+  final String image;
+  final String title;
+  final bool isActive;
+
+  const SympthomCards({
+    Key key,
+    this.image,
+    this.title,
+    this.isActive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white,
+          boxShadow: [
+            isActive
+                ? BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 20,
+                    color: kActiveShadowColor,
+                  )
+                : BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 3,
+                    color: kShadowColor,
+                  ),
+          ]),
+      child: Column(
+        children: <Widget>[
+          Image.asset(
+            image,
+            height: 80,
+          ),
+          Text(
+            "$title",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 10),
+          ),
+        ],
+      ),
+    );
+  }
+}
